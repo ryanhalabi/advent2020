b878c25722bfb9ad75c76fe3b2d1e591d3ffb0f0
Ryan Halabi <ryanhalabi@gmail.com>
2020-12-01 18:41:05 -0700

    general solution

--- 1/solution.py
+++ 1/solution.py
@@ -1,42 +1,19 @@
-# def find_n_value(x, n, target):
-
-#     for a in x:
-#         new_target = target - a
-
-#         value = find_n_value(x.pop(a), n-1, new_target)
-
-#         if value:
-#             return value * a
-
-#     return None
-
-
-def two_num_finder(x, value=2020):
-    """
-    searches list x for 2 values that sum to value and returns their product
-    """
-    x = set(x)
-    for y in x:
-        complement = value - y
-        if complement in x:
-            return complement * y
-    return None
-
-
-def three_num_finder(x, value=2020):
-    """
-    searches list x for 3 values that sum to value and returns their product
-    """
-    x = set(x)
-    for y in x:
-        complement = value - y
-        z = two_num_finder(x, value=complement)
-        if z:
-            return z * y
-
-
 my_file = open("data_1.csv", "r")
 x = [int(x.replace("\n", "")) for x in my_file.readlines()]
 
-y = two_num_finder(x)
-z = three_num_finder(x)
+
+def find_n_value(expenses, target=2020, n=3):
+    if n == 1 and target in expenses:
+        return [target]
+    elif n == 1:
+        return None
+    while expenses:
+        x = expenses.pop()
+        c = target - x
+        if found := find_n_value(expenses[:], target=c, n=n - 1):
+            found.append(x)
+            return found
+
+
+two = find_n_value(x.copy(), 2020, 2)
+three = find_n_value(x.copy(), 2020, 3)
