83d9bfdf6955422800c9b27b6714e4c78bf37d18
Ryan Halabi <ryanhalabi@gmail.com>
2020-12-01 18:03:01 -0700

    initial solutions

--- 1/solution.py
+++ 1/solution.py
@@ -1,8 +1,42 @@
-def num_finder(x):
+# def find_n_value(x, n, target):
+
+#     for a in x:
+#         new_target = target - a
+
+#         value = find_n_value(x.pop(a), n-1, new_target)
+
+#         if value:
+#             return value * a
+
+#     return None
+
+
+def two_num_finder(x, value=2020):
+    """
+    searches list x for 2 values that sum to value and returns their product
+    """
     x = set(x)
     for y in x:
-        complement = 2020 - y
+        complement = value - y
         if complement in x:
             return complement * y
-x = [1721, 979, 366, 299, 675, 1456]
-y = num_finder(x)
+    return None
+
+
+def three_num_finder(x, value=2020):
+    """
+    searches list x for 3 values that sum to value and returns their product
+    """
+    x = set(x)
+    for y in x:
+        complement = value - y
+        z = two_num_finder(x, value=complement)
+        if z:
+            return z * y
+
+
+my_file = open("data_1.csv", "r")
+x = [int(x.replace("\n", "")) for x in my_file.readlines()]
+
+y = two_num_finder(x)
+z = three_num_finder(x)
